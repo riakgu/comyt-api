43f4bb597da489ed8f7bcfbd86e4745c10eb68a4
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-12-31 19:08:30 +0700

    chore: setup winston logger

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,13 @@
 import {app} from './config/express';
 import {config} from "./config";
 import type {Request, Response} from "express";
+import {logger} from "./config/logger";
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
 app.listen(config.app.port, config.app.host, () => {
-    console.log(`Server running at http://${config.app.host}:${config.app.port}`);
-    console.log(`Environment: ${config.app.env}`);
+    logger.info(`Server running at http://${config.app.host}:${config.app.port}`);
+    logger.info(`Environment: ${config.app.env}`);
 });

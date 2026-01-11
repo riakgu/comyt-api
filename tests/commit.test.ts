820853860ab70e84b632e154a6bfe71ba0eb1cc1
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-01-11 08:25:19 +0700

    fix: close Redis connection after tests to prevent Jest hanging

--- tests/commit.test.ts
+++ tests/commit.test.ts
@@ -1,5 +1,10 @@
 import supertest from "supertest";
 import { app } from "../src/config/express";
+import { redis } from "../src/config/redis";
+
+afterAll(async () => {
+    await redis.quit();
+});
 
 describe('POST /api/commits/generate', () => {
 
